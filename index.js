c62dce2e591e59f5a3090f7402682e46bf3a130d
Timothy Hedge <timwhedge@gmail.com>
2019-01-20 22:35:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function updateObjectWithKeyAndValue(object, key, value)
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 {
-  object[key] = value
+  return object[key] = value
 }
