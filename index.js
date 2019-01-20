5cfa294e62246df363ab853b2b510d3c47080fbf
Timothy Hedge <timwhedge@gmail.com>
2019-01-20 22:41:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 
 function deleteFromObjectByKey(object, key)
 {
-  return Object.assign( {}, object, delete object[key] )
+  return Object.assign( {}, delete object[key] )
 }
